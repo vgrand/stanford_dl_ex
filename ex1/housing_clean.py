0fa632ebd682c0af3751a85e9069662eb8d387ef
vgrand <valentin.grand@gmail.com>
2016-08-29 23:17:02 +0100

    comments

--- ex1/housing_clean.py
+++ ex1/housing_clean.py
@@ -1,5 +1,11 @@
 import re
 
+"""
+Formatting the housing.data file to replac all spaces by commas where appropriate:
+ - Get rid of all spaces starting lines
+ - Replace the remaining spaces with commas
+"""
+
 with open('housing.data', 'rb') as f:
 	with open('housing.csv', 'wb') as g:
 		g.write( re.sub( ' +', ',' ,re.sub( '^ +', '', re.sub('\n +', '\n', f.read()))))
